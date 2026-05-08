901ad00b6308c0e2bbd8fdbb30a0e0930e65dc98
Seann-dev <tbakunn12@gmail.com>
2026-05-09 05:30:22 +0700

    first upload

---
+++ bot.py
@@ -0,0 +1,43 @@
+import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+
+BOT_TOKEN = "8614178412:AAH9Xj7f3mi-wcWh2fwvYEdweO6x4jQJq6k"
+
+DOWNLOAD_DIR = "downloads"
+
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    msg = await update.message.reply_text("Downloading...")
+
+    try:
+        ydl_opts = {
+            'outtmpl': f'{DOWNLOAD_DIR}/%(title)s.%(ext)s',
+            'format': 'mp4/bestaudio/best',
+            'noplaylist': True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            file_path = ydl.prepare_filename(info)
+
+        await update.message.reply_document(document=open(file_path, 'rb'))
+
+        os.remove(file_path)
+
+    except Exception as e:
+        await update.message.reply_text(f"Error: {e}")
+
+    await msg.delete()
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
+
+print("Bot jalan...")
+
+app.run_polling()
